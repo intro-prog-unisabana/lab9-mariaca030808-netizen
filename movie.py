0bb7ede06f2910a37e4f0162f999ad56a25f2f4c
Camila Murcia <mariamurmu@unisabana.edu.co>
2026-05-04 22:22:30 -0500

    movie prueba 1

--- movie.py
+++ movie.py
@@ -7,7 +7,8 @@ class Movie:
         self.year = year
 # FREEZE CODE END
     # TODO: Define the __str__ method!
-
+    def __str__(self):
+        return f"Movie: {self.title} (Directed by {self.director}, {self.year})"
 
 # FREEZE CODE BEGIN
 if __name__ == "__main__":
@@ -20,3 +21,5 @@ if __name__ == "__main__":
     
     # TODO: Construct a Movie object!
     # TODO: Print the object!
+    my_movie = Movie(title, director, year)
+print(my_movie)
--- song.py
+++ song.py
@@ -4,9 +4,7 @@ class Song:
         self.name= name       
         self.artist= artist    
         self.length= length   
- 
     def get_length_in_seconds(self):
         return self.length * 60
- 
     def __str__(self):
         return f"'{self.name}' by {self.artist} ({self.length})"
